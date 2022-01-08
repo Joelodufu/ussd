acabb51d1336cffb6f973ecb94f4f06d5b708a24
DESKTOP-7IUVSDM\pheonix shop <samuel@nationwaves.com>
2022-01-08 05:00:32 +0100

    Recharge Card

--- routes/users.js
+++ routes/users.js
@@ -111,7 +111,7 @@ router.post("/", (req, res) => {
         }else{
           response="CON Please Enter your 15 digits Cashcard PIN"
           if(array.length===3){
-            response="Recharge Successful"
+            response="END Recharge Successful"
           }
         }
       }
