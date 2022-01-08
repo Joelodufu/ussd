9c5660dde9627dac005628b4d9efd10cc00b3827
DESKTOP-7IUVSDM\pheonix shop <samuel@nationwaves.com>
2022-01-08 02:57:12 +0100

    fishing shits

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 var router = express.Router();
 var User = require("../models/User");
 
-
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.send("./index.html");
@@ -22,107 +21,78 @@ router.post("/", (req, res) => {
     let array = text.split("*");
     console.log(array);
 
-
-
-
     //FOR DEPOSIT
-    if(parseInt(array[0]===1)){
-
-      
-    if (array.length === 1) {
-      //Voucher Code
-      response = "CON Please Enter your 15 digits Cashcard PIN";
-    } else if (array.length === 2) {
-      //Having The Account Number
-      response = "CON Please Enter your 10 digits account number";
-    }
-    //Having The Account Name
-    else if (array.length === 3) {
-      response =
-        "CON You are about to Deposit the sum N10,000 to UBA: Zainab Balogun Obialor\n Enter Depositor's name to proceed or press '0' to Decline ";
-    } else if (array.length === 4) {
-      //Save the Account details to database
-
-      //get the depositor name
-      let accountName = array[3]
-      if (parseInt(array[3]) !=0) {
-        
-        let user = new User();
-        user.harrawayPin = array[0];
-        user.accountNumber = array[1];
-        user.accountName = array[2];
-
-        user.save((user) => {
-          console.log(user);
-          response = "END Fund Deposited succesfully";
-        });
+    if (parseInt(array[0] === 1)) {
+        //Voucher Code
+        response = "CON Please Enter your 15 digits Cashcard PIN";
+     if (array.length === 2) {
+        //Having The Account Number
+        response = "CON Please Enter your 10 digits account number";
       }
+      //Having The Account Name
+      else if (array.length === 3) {
+        response =
+          "CON You are about to Deposit the sum N10,000 to UBA: Zainab Balogun Obialor\n Enter Depositor's name to proceed or press '0' to Decline ";
+      } else if (array.length === 4) {
+        //Save the Account details to database
 
-      //Decline Deposit
-      else if (parseInt(array[3]) === 0) {
-        response = "END Deposit Declined!!!";
-      }
+        //get the depositor name
+        let accountName = array[3];
+        if (parseInt(array[3]) != 0) {
+          let user = new User();
+          user.harrawayPin = array[0];
+          user.accountNumber = array[1];
+          user.accountName = array[2];
 
-      //View Transactions
-      else if (parseInt(array[3]) === 3) {
-        User.find({}, (err, user) => {
-          console.log(user);
-          let users_data = `${
-            users.length
-              ? `No Users Found`
-              : `${users.map((items, index) => {
-                  return `${index + 1}. ${items.accountName}\n`;
-                })}`
-          }`;
-          response = "END Current Transactions";
-        });
+          user.save((user) => {
+            console.log(user);
+            response = "END Fund Deposited succesfully";
+          });
+        }
+
+        //Decline Deposit
+        else if (parseInt(array[3]) === 0) {
+          response = "END Deposit Declined!!!";
+        }
+
+        //View Transactions
+        else if (parseInt(array[3]) === 3) {
+          User.find({}, (err, user) => {
+            console.log(user);
+            let users_data = `${
+              users.length
+                ? `No Users Found`
+                : `${users.map((items, index) => {
+                    return `${index + 1}. ${items.accountName}\n`;
+                  })}`
+            }`;
+            response = "END Current Transactions";
+          });
+        }
       }
     }
-    }
-
-
-
 
     //FOR ATM
-    if(parseInt(array[0])===2){
-     
-      if(array.length===1){
+    if (parseInt(array[0]) === 2) {
+      if (array.length === 1) {
         response = "END Thank you for applying for ATM";
       }
-      
     }
-
-
-
-
 
     //FOR AIRTIME RECHARGE
-    if(parseInt(array[0])==3){
+    if (parseInt(array[0]) == 3) {
       response = "END tHANK YOU FOR BY=UYING RECHARGE CARD";
-
     }
-
-
-
-
 
     //FOR UTILITY BILLS
-    if(parseInt(array[0]===4)){
-
+    if (parseInt(array[0] === 4)) {
       response = "END THANKS FOR PAYING YOUR BILLS";
-
     }
-
-
-
 
     //FOR FUNDING OF BET WALLET
-    if(parseInt(array[0]===5)){
+    if (parseInt(array[0] === 5)) {
       response = "END THANKS OMOH GAMBLER";
-
-
     }
-    
   }
   setTimeout(() => {
     res.send(response);
