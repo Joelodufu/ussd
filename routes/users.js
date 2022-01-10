8c1ff93b2a628dc297a11b380cd6bcf2b618e360
DESKTOP-7IUVSDM\pheonix shop <samuel@nationwaves.com>
2022-01-10 04:42:22 +0100

    try second airtime

--- routes/users.js
+++ routes/users.js
@@ -36,19 +36,7 @@ router.post("/", (req, res) => {
           "CON You are about to Deposit the sum N10,000 to UBA: Zainab Balogun Obialor\n Enter Depositor's name to proceed or press '0' to Decline ";
       } else if (array.length === 4) {
         //Save the Account details to database
-
-        //get the depositor name
-        let accountName = array[3];
-        if (parseInt(array[3]) != 0) {
-          let user = new User();
-          user.harrawayPin = array[0];
-          user.accountNumber = array[1];
-          user.accountName = array[2];
-
-          user.save((user) => {
-            console.log(user);
-            response = "END Fund Deposited succesfully";
-          });
+        response = "END Fund Deposited succesfully";
         }
 
         //Decline Deposit
